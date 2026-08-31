5734cdb612b30ee4a171217f20c0af79ee4e2563
Melaninmaps <2.94724305e+08+Melaninmaps@users.noreply.github.com>
2026-08-31 17:29:29 +0000

    release: publish product repair artifacts

--- artifacts/api-server/src/generated/spaHtml.ts
+++ artifacts/api-server/src/generated/spaHtml.ts
@@ -83,8 +83,8 @@ export const SPA_HTML = `<!DOCTYPE html>
         _reportErr('UnhandledRejection: ' + (err && err.message ? err.message : String(err)), location.href, 0, 0, err);
       });
     </script>
-    <script type="module" crossorigin src="/assets/index-BCnPMlLn.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/index-CFksxbEc.css">
+    <script type="module" crossorigin src="/assets/index-DC4BS1Tk.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-C3oDJVHs.css">
   </head>
   <body>
     <div id="root"></div>
