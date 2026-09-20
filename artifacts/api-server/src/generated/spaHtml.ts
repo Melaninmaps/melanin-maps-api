16348f5ebab08f8b8028c4af6ac51752c134d5ae
Melaninmaps <melanin_maps@yahoo.com>
2026-09-20 06:36:06 +0000

    chore: sync release runtime for 3eaade7f

--- artifacts/api-server/src/generated/spaHtml.ts
+++ artifacts/api-server/src/generated/spaHtml.ts
@@ -82,8 +82,8 @@ export const SPA_HTML = `<!DOCTYPE html>
         _reportErr('UnhandledRejection: ' + (err && err.message ? err.message : String(err)), location.href, 0, 0, err);
       });
     </script>
-    <script type="module" crossorigin src="/assets/index-9XBA9xT9.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/index-y49rA4vB.css">
+    <script type="module" crossorigin src="/assets/index-C7FXRocF.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-M4kdUhwe.css">
   </head>
   <body>
     <div id="root"></div>
