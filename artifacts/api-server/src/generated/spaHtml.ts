e6444197cd1aee6c613371fd52ed0ffaa185c932
Melaninmaps <2.94724305e+08+Melaninmaps@users.noreply.github.com>
2026-09-25 16:59:37 +0000

    build refresh Admin inventory controls static bundle

--- artifacts/api-server/src/generated/spaHtml.ts
+++ artifacts/api-server/src/generated/spaHtml.ts
@@ -82,8 +82,8 @@ export const SPA_HTML = `<!DOCTYPE html>
         _reportErr('UnhandledRejection: ' + (err && err.message ? err.message : String(err)), location.href, 0, 0, err);
       });
     </script>
-    <script type="module" crossorigin src="/assets/index-Cx4yusTY.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/index-11mMFzzM.css">
+    <script type="module" crossorigin src="/assets/index-Bvv3OyDf.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-BaGo8oXC.css">
   </head>
   <body>
     <div id="root"></div>
