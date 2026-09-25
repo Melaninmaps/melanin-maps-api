dc2851c62ce13faea1261fe51503369b4eeba4fc
Melaninmaps <2.94724305e+08+Melaninmaps@users.noreply.github.com>
2026-09-25 06:03:52 +0000

    chore: sync Admin retention static bundle

--- artifacts/api-server/src/generated/spaHtml.ts
+++ artifacts/api-server/src/generated/spaHtml.ts
@@ -82,8 +82,8 @@ export const SPA_HTML = `<!DOCTYPE html>
         _reportErr('UnhandledRejection: ' + (err && err.message ? err.message : String(err)), location.href, 0, 0, err);
       });
     </script>
-    <script type="module" crossorigin src="/assets/index-BLV5xwFa.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/index-DHFxFmwb.css">
+    <script type="module" crossorigin src="/assets/index-C4RFV02S.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-e_epnUxz.css">
   </head>
   <body>
     <div id="root"></div>
